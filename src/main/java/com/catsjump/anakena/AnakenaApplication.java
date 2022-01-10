0992ebd29c2b85fb18bc436e3a4fe2b00ceba070
Tales Marland <marland.tales@gmail.com>
2022-01-10 18:26:53 -0300

    Projeto criado

---
+++ src/main/java/com/catsjump/anakena/AnakenaApplication.java
@@ -0,0 +1,13 @@
+package com.catsjump.anakena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnakenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnakenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catsjump/anakena/AnakenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catsjump.anakena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnakenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
