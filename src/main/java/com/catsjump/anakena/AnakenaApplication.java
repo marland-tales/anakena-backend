24c997e7985cdfc365e2331832ee63ef1d2e61a6
Tales Marland <marland.tales@gmail.com>
2022-01-18 19:31:30 -0300

    Criando operacao de instanciacao

--- src/main/java/com/catsjump/anakena/AnakenaApplication.java
+++ src/main/java/com/catsjump/anakena/AnakenaApplication.java
@@ -1,13 +1,32 @@
 package com.catsjump.anakena;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AnakenaApplication {
+import com.catsjump.anakena.domain.Categoria;
+import com.catsjump.anakena.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AnakenaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AnakenaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
